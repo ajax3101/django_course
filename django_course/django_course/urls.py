17a8450cbd6cab84fdddf880794402164d92d6b3
Denys Morozov <ajax3101@gmail.com>
2023-04-08 09:55:03 +0300

    add url path lesson 3 and lesson 5

--- django_course/django_course/urls.py
+++ django_course/django_course/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('lesson_11/', include('lesson_11.urls')),
     path('lesson_1_1/', include('lesson_11.urls')),
     path('lesson_2/', include('lesson_2.urls')),
+    path('lesson_3/', include('lesson_3.urls')),
+    path('lesson_5/', include('lesson_5.urls'))
 ]
